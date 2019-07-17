b994e257d7c8fd30db5b45c260219a63e2729bf9
Nahuel Torres <ntorres@kolektor.com.ar>
2019-07-17 18:01:40 -0300

    Se empezo con el training de API testing para REST

---
+++ Sample API Testing Project/Object Repository/User/GetUserDetails.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>GetUserDetails</name>
+   <tag></tag>
+   <elementGuidId>d6718605-9fd2-4542-b61e-1937b46de516</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>https://reqres.in/api/users/2</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceFunction></soapServiceFunction>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
---
+++ Sample API Testing Project/Test Suites/UserTestSuite/UserValidation.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>UserValidation</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>b98b6337-0340-42b9-ab24-915c38c56d37</testSuiteGuid>
+   <testCaseLink>
+      <guid>a542719e-4b39-4b21-bccd-862b12d93f0c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UserTestCases/Validate User Details</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
